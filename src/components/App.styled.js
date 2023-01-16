e5d1a66f0bc5ec9234b942f5ca7962de95b07087
Toma1402 <toma.k1402@gmail.com>
2023-01-16 17:10:45 +0200

    add style

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 export const Phonebook = styled.div`
   width: 400px;
   margin-left: 20px;
+  margin-top: 20px;
   padding: 15px;
   border: 1px solid black;
   border-radius: 10px;
